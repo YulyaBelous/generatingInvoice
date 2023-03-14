13ed228e71536d080229732c7c3e168b26e4709d
Yuliya <yulya.al.belous@gmail.com>
2023-03-14 19:53:43 +0200

    Initial version of generatingInvoice generated by generator-jhipster@7.9.3

---
+++ src/main/java/org/example/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package org.example.repository;
+
+import org.example.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/org/example/IntegrationTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.example.GeneratingInvoiceApp;
+import org.example.config.AsyncSyncConfiguration;
+import org.example.config.EmbeddedSQL;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { GeneratingInvoiceApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
